62809d557ceba4b349971d7ffdc9e5faa9ee4cb3
Tyler Wright <tylerwright@MacBook-Air.local>
2024-09-23 22:16:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double winterCost = 40.01;
+        double springCost = 30.00;
+        double summerCost = 20.99;
+        double fallCost = 8.01;
+        double totalCost = 0.00;
+        
+    totalCost = summerCost + fallCost + winterCost + springCost;
+    System.out.println("Your Total Maintinence Cost is " + totalCost);
+
+    }
+}
